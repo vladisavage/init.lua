172f8431b83db2f746c4ab37d7248382ac273607
vladisavage <vlad.ghost@gmail.com>
2023-12-09 19:56:31 +0200

    change telescope width

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,15 @@
+require('telescope').setup({
+  defaults = {
+    layout_config = {
+        horizontal = {
+            width = 0.98,
+            height = 0.98,
+            preview_width = 0.4,
+        },
+    },
+  },
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
