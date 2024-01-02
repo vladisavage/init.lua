3caa6814a0e9e9c99c0d3b14125c56ba1311546e
Vladislav Proskurov <vlad.ghost@gmail.com>
2024-01-03 00:35:21 +0200

    add gll support

--- lua/0xVladi/remap.lua
+++ lua/0xVladi/remap.lua
@@ -5,3 +5,22 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("v", "<leader>y", '"+y', { noremap = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true })
+
+-- for zsh gll (git log) --
+vim.api.nvim_create_autocmd({"FileType"}, {
+    pattern = {"gitcommit", "gitrebase", "git"},
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, 'n', 'K', ':lua ShowGitCommitDiffInTmux()<CR>', {noremap = true, silent = true})
+    end
+})
+
+function ShowGitCommitDiffInTmux()
+    local word = vim.fn.expand("<cword>")
+    if #word == 40 and string.match(word, "%x+") then
+        local tmux_cmd = 'tmux new-window "git show ' .. word .. ' | delta; echo Press enter to continue...; read"'
+        vim.cmd('! ' .. tmux_cmd)
+    else
+        vim.cmd('normal! K')
+    end
+end
+        
