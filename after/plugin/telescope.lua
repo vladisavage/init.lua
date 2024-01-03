53a596f34c54c9ee466b8ee8abd09fd1ca31adc9
Vladislav Proskurov <vlad.ghost@gmail.com>
2024-01-03 20:40:37 +0200

    telescope: change find files to <leader>o

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,7 +11,7 @@ require('telescope').setup({
 })
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>p', builtin.find_files, {})
+vim.keymap.set('n', '<leader>o', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>g', builtin.git_files, {})
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
